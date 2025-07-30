6acef408026cc410e7efeed5655ce999fad59571
prabhu-woyce <pparmar@woyce.io>
2025-07-30 12:10:33 +0530

    #update AllRooms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 import Footer from "./components/Footer";
+import AllRooms from "./pages/AllRooms";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("/owner");
@@ -14,6 +15,7 @@ const App = () => {
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/rooms" element={<AllRooms />} />
         </Routes>
       </div>
       <Footer/>
